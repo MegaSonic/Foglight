66d3772bfcb119bf3c1477cf5b5a747d54526a7d
jmbiggs <u0413046@1110mac-36.lib.utah.edu>
2015-02-19 18:52:52 -0700

    trying again

---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class World : MonoBehaviour {
+
+	public int amtOfSquares;
+
+	// we'll obvious do this differently
+	public int unlockedLevels;
+
+	[HideInInspector]
+	private float[] availableHope;
+
+	// Use this for initialization
+	void Start () {
+		availableHope = new float[amtOfSquares];
+	}
+
+	// Adds given amt to given level's available hope
+	public void AddHope (int level, float amt){
+		availableHope[level-1] += amt;
+	}
+
+	// Returns the possible amount of hope available in given level
+	public float GetHope (int level){
+		return availableHope [level - 1];
+	}
+
+	// Returns the total possible amount of hope
+	//   given currently unlocked levels
+	public float GetHope () {
+		float result = 0;
+		for (int i = 0; i<unlockedLevels; i++) {
+			result += GetHope (i+1);
+		}
+		return result;
+	}
+
+}
