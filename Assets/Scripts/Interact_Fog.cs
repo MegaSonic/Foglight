27ee65329470d42eca01c4a87d653086682c9227
Trezym <u0798664@kcmac-42.scl.utah.edu>
2015-02-26 18:01:12 -0700

    Created FogWall and Struggled with Git

---
+++ Assets/Scripts/Interact_Fog.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interact_Fog : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider col)
+	{
+		// if you're touching fog and you press the "interact" key
+		if(col.gameObject.tag=="fog" && Input.GetKeyDown("return"))
+		{
+			// make wall intangible
+			Destroy(col.gameObject.transform.parent.gameObject.transform.FindChild("Collider").gameObject);
+			// deactivate particle system
+			col.gameObject.transform.parent.gameObject.particleSystem.enableEmission = false;
+		}
+			
+	}
+}
