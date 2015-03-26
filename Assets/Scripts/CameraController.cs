29b782f23d69c72b7db77c61c50f4f0a506e72f2
Michael Biggs <eraserhead@gmail.com>
2015-03-25 21:21:32 -0600

    made some improvements to camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,6 +6,12 @@ public class CameraController : MonoBehaviour {
 
 	public float distance;
 	public float height;
+	public float runDistance;
+	public float runHeight;
+	public float lookDownHeight;
+	public float lookDownDistance;
+	public float lookUpHeight;
+	public float lookUpDistance;
 	public float manualRotateSpeed; // speed of joystick rotation
 	public float moveSpeed;
 	public float rotateSpeed; // speed of automatic rotation
@@ -13,9 +19,6 @@ public class CameraController : MonoBehaviour {
 
 	private Transform cam;
 	private Transform target;
-
-	private bool inputLastPressed = false; // true when movement input was more recent than rotation input
-
 
 	// Use this for initialization
 	void Awake () {
@@ -26,33 +29,67 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		// rotate the camera around the player
+		
+		
+		//// rotate the camera around the player manually ////
+		
 		if (Mathf.Abs (Input.GetAxis ("CamHorizontal")) > 0.1f) {						
-			inputLastPressed = false;
 			cam.RotateAround (target.position, Vector3.up, Input.GetAxis ("CamHorizontal") * manualRotateSpeed);
-		}
-		//if (Input.GetAxis ("CamVertical")) {	}
-
-
-		// set bool and rotate player to match camera rotation
-		else if (Mathf.Abs (Input.GetAxis ("Horizontal")) > 0.1f ||
-				 Mathf.Abs (Input.GetAxis ("Vertical")) > 0.1f) {
-			inputLastPressed = true;
-			target.rotation = cam.rotation;
+			// rotate player to match camera rotation		
+			// maybe we want to be able to see the front of it sometimes though?
+			target.LookAt (cam.position);
+			target.RotateAround(target.position, Vector3.up, 180f);
 		}
 
-		// smoothly move camera behind player
-		if (inputLastPressed) {
+		//// Rotate gradually to face the player ////	
+		else
+		{
 			var targetRotation = Quaternion.LookRotation(target.position - cam.position);
 			cam.rotation = Quaternion.Slerp(cam.rotation, targetRotation, rotateSpeed * Time.deltaTime);
-
-			Vector3 targetPosition = target.forward * -distance;
-			targetPosition.x += target.position.x;
-			targetPosition.z += target.position.z;
-			targetPosition.y = height;
-
-			cam.position = Vector3.Lerp (cam.position, targetPosition, moveSpeed * Time.deltaTime);
-			//cam.position = Vector3.Lerp (cam.position, new Vector3 (target.position.x, cam.position.y, target.position.z - distance), speed * Time.deltaTime);	
 		}
+
+		//// Move gradually toward player ////
+
+		// start by setting target position to player's
+		Vector3 targetPosition = target.position;
+
+		// adjust distance and height of target based upon whether player is running
+		if (Input.GetButton ("Run")) 
+		{
+			targetPosition += target.forward * -runDistance;
+			targetPosition.y += runHeight;
+		}
+		else
+		{
+			targetPosition += target.forward * -distance;
+			targetPosition.y += height;
+		}
+
+		// make further adjustments based upon vertical camera joystick input
+
+		// look up - move forward and down
+		if (Input.GetAxis ("CamVertical") < -0.1f) 
+		{	
+			targetPosition.y += lookUpHeight * Input.GetAxis ("CamVertical");
+			targetPosition += target.forward * -lookUpDistance * Input.GetAxis ("CamVertical");
+			// rotate up a bit more
+			//var upRotation = Quaternion.LookRotation(target.up, target.up);
+			Vector3 adjTarget = target.position;
+			adjTarget.y += lookUpHeight;
+			var newTargetRotation = Quaternion.LookRotation(adjTarget - cam.position);
+			cam.rotation = Quaternion.Slerp(cam.rotation, newTargetRotation, rotateSpeed * Time.deltaTime);
+
+		}
+		// look down - move back and up
+		else if (Input.GetAxis ("CamVertical") > 0.1f) 
+		{	
+			targetPosition.y += lookDownHeight * Input.GetAxis ("CamVertical");
+			targetPosition += target.forward * -lookDownDistance * Input.GetAxis ("CamVertical");
+
+		}
+
+		// do the actual moving
+		cam.position = Vector3.Lerp (cam.position, targetPosition, moveSpeed * Time.deltaTime);
+
 	}
 }
