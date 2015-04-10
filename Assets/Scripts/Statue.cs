d758ae34cdb26c03fcfcd87b7aa62f0aa6872e70
jmbiggs <eraserhead@gmail.com>
2015-04-09 18:34:42 -0600

    Improved statue script

--- Assets/Scripts/Statue.cs
+++ Assets/Scripts/Statue.cs
@@ -48,6 +48,8 @@ public class Statue : MonoBehaviour {
 	private int bookNum = 0;
 
 	private World world;
+	private SpringFollow sf;
+	private RotationControl rc;
 	private PlayerStats ps;
 	private Canvas can;
 	private Text nameDisplay;
@@ -65,6 +67,9 @@ public class Statue : MonoBehaviour {
 	void Awake() {
 		playerParticles = GameObject.FindGameObjectWithTag("Particle").GetComponent<ParticleSystem>();
 
+		sf = GameObject.FindGameObjectWithTag ("Player").transform.parent.gameObject.GetComponentInChildren<SpringFollow> ();
+		rc = GameObject.FindGameObjectWithTag ("Player").transform.parent.gameObject.GetComponentInChildren<RotationControl> ();
+
 		world = FindObjectOfType<World> ();
 		ps = FindObjectOfType<PlayerStats> ();
 		nameFog = GameObject.FindGameObjectWithTag("NameFog").GetComponent<ParticleSystem>();
@@ -73,9 +78,9 @@ public class Statue : MonoBehaviour {
 
 		// Temp Camera Stuff
 		playerColor = playerParticles.startColor;
-		playerHeight = GameObject.FindGameObjectWithTag("Player").transform.parent.gameObject.GetComponentInChildren<SpringFollow>().height;
-		camDistance = GameObject.FindGameObjectWithTag("Player").transform.parent.gameObject.GetComponentInChildren<RotationControl> ().distance;
-		camHeight = GameObject.FindGameObjectWithTag("Player").transform.parent.gameObject.GetComponentInChildren<RotationControl> ().height;
+		playerHeight = sf.height;
+		camDistance = rc.distance;
+		camHeight = rc.height;
 
 	}
 
@@ -193,19 +198,20 @@ public class Statue : MonoBehaviour {
 		openNewestBook ();
 
 		// Temp Camera Stuff
-		GameObject.FindGameObjectWithTag("Player").transform.parent.gameObject.GetComponentInChildren<SpringFollow>().height = 0;
-		GameObject.FindGameObjectWithTag("Player").transform.parent.gameObject.GetComponentInChildren<RotationControl> ().distance = 6f;
-		GameObject.FindGameObjectWithTag("Player").transform.parent.gameObject.GetComponentInChildren<RotationControl> ().height = 1.5f;
+		sf.height = 0;
+		rc.distance = 6f;
+		rc.height = 1.5f;
 	}
 
 	void OnTriggerExit(Collider other){
 		playerParticles.startColor = playerColor;
-		clearDialog ();	
 
 		// Temp Camera Stuff
-		GameObject.FindGameObjectWithTag("Player").transform.parent.gameObject.GetComponentInChildren<SpringFollow>().height = playerHeight;
-		GameObject.FindGameObjectWithTag("Player").transform.parent.gameObject.GetComponentInChildren<RotationControl> ().distance = camDistance;
-		GameObject.FindGameObjectWithTag("Player").transform.parent.gameObject.GetComponentInChildren<RotationControl> ().height = camHeight;
+		sf.height = playerHeight;
+		rc.distance = camDistance;
+		rc.height = camHeight;
+
+		clearDialog ();	
 	}
 
 	void OnTriggerStay(Collider other){
